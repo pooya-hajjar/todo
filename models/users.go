7bc92881766a8fae3559f00606a69056df692801
pooya hajjar <pooya.hhz@gmail.com>
2024-03-13 11:58:17 +0330

    fix : update user model

--- models/users.go
+++ models/users.go
@@ -1,13 +1,15 @@
 package models
 
+import "database/sql"
+
 type Users struct {
-	ID        int
-	UserName  string
-	Password  string
-	CreatedAt uint
-	UpdatedAt uint
-	DeletedAt uint
-	Status    int
-	Email     string
-	Avatar    string
+	ID        int            `json:"id"`
+	UserName  string         `json:"username"`
+	Password  string         `json:"password"`
+	CreatedAt sql.NullTime   `json:"created_at"`
+	UpdatedAt sql.NullTime   `json:"updated_at"`
+	DeletedAt sql.NullTime   `json:"deleted_at"`
+	Status    int            `json:"status"`
+	Email     sql.NullString `json:"email"`
+	Avatar    sql.NullString `json:"avatar"`
 }
